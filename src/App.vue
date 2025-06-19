a2d319bf4c992d5cbb0307b5f970890b2f7a7347
Rin <64449865+lee-haell@users.noreply.github.com>
2025-06-19 11:51:41 +0900

    [Add] 사용자 이름 입력 및 버튼 영역

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="!hasEntered">
+    <input
+      v-model="username"
+      @keyup.enter="confirmName"
+      placeholder="이름을 입력하세요"
+    />
+    <button @click="confirmName">Enter</button>
+  </div>
+  
+  <!-- v-if="hasEntered" 이 조건에 따라 화면이 갈림 -->
+  <div v-if="hasEntered"> 
+    <p>Hello, {{ username }}!</p>
+
+    <input
+      v-model="newTask"
+      @keyup.enter="addTask"
+      placeholder="할 일을 입력하세요"
+    />
+
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default { //이 파일이 vue 컴포넌트라고 알려주는 코드
+  name: 'App', //컴포넌트의 이름 (디버깅/확장에 유용)
+  data(){
+    return {
+      username : '',
+      hasEntered: false, //초기값
+      newTask: '',
+      tasks: [],
+    }
+  },
+  methods: {
+    confirmName() {
+      if (this.username.trim()) {
+        this.hasEntered = true;
+      }
+    }
+  },
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
   }
 }
 </script>
